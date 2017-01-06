fe133305f2d1f23c1626b96af4a644aab36823ca
jiaozongguan <jiao_zg22@163.com>
2017-01-06 23:12:39 +0800

    超类的newInstance获得的对象，是Object类型的，不能直接具体到哪个超类

---
+++ src/main/java/com/jzg/tingkingInJava/test/RTTI/toy/GenericToyTest.java
@@ -0,0 +1,18 @@
+package com.jzg.tingkingInJava.test.RTTI.toy;
+
+public class GenericToyTest {
+
+	public static void main(String[] args) throws Exception {
+
+		Class<FancyToy> ftClass = FancyToy.class;
+		//Produces exact type
+		FancyToy fancyToy = ftClass.newInstance();
+		Class<? super FancyToy> up = ftClass.getSuperclass();
+		//This won't compile
+		//Class<Toy> up2 = ftClass.getSuperclass();
+		//Only produces Object
+		Object obj = up.newInstance();
+		Toy toy = (Toy) up.newInstance();
+	}
+
+}
