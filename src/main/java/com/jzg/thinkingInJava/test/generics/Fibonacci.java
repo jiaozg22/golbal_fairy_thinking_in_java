f280bfd81033429d338d6d0d667fce575a82e0a1
jzg <jiao_zg22@163.com>
2018-01-07 20:54:06 +0800

    人为冲突，讲解

--- src/main/java/com/jzg/thinkingInJava/test/generics/Fibonacci.java
+++ src/main/java/com/jzg/thinkingInJava/test/generics/Fibonacci.java
@@ -35,6 +35,7 @@ public class Fibonacci implements Generator<Integer> {
 	}
 
 	public static void main(String[] args) {
+		//修改master分支
 		Fibonacci fibonacci = new Fibonacci();
 		System.out.println("18的菲波那切数列:" + fibonacci.getFibonacci(18) + "."
 				+ "getFibonacci方法被调用过的次数" + fibonacci.allCount);
